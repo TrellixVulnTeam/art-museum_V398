f6e7b54d1e6bc4645106b8162e888e76dd394bb9
Egnate Noniashvili <103636296+egnointhewoods@users.noreply.github.com>
2022-08-16 14:04:42 +0000

    Add navbar

--- src/Components/BackgroundImage.tsx
+++ src/Components/BackgroundImage.tsx
@@ -1,6 +1,4 @@
 import "../Component-Styles/backgroundImage.scss";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { motion } from "framer-motion"
 import { useState } from "react";
 export default function BackgroundImage() {
 	const [isLoaded, setIsLoaded] = useState(false);
@@ -9,22 +7,7 @@ export default function BackgroundImage() {
 		<div className="backgroundImage" onLoad={() => setIsLoaded(true)}
 		>
 			<div className="imageText">
-				<div>
-					<p className="quoteParagraph p-1">
-						One Small Step For Man.
-					</p>
-					<p className="quoteParagraph p-2">
-						One Giant Leap For Mankind.
-					</p>
-				</div>
-				<motion.a
-					href="#main" id="backgroundImageAnchor"
-					initial={{ opacity: 0 }} whileHover={{ scale: 1.2, transition: { duration: 1 }, }}
-					whileTap={{ scale: 0.9 }}
-					whileInView={{ opacity: 1 }}>
-					Explore The Universe
-					<KeyboardArrowDownIcon />
-				</motion.a>
+				<h2>We preserve History To build Future</h2>
 			</div>
 		</ div>
 	);
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,8 +1,83 @@
+import { useState, useEffect } from "react";
 import "../Component-Styles/navbar.scss";
-export default function Navbar() {
+
+export default function Navbarr() {
+	const [isHamburgerPressed, setIsHamburgerPressed] = useState<boolean>(false)
+	const [scrollDir, setScrollDir] = useState<any>(null);
+	function hamburgerPressFunction() {
+
+		if (isHamburgerPressed) {
+			setIsHamburgerPressed(false);
+		} else {
+			setIsHamburgerPressed(true);
+		}
+	}
+	useEffect(() => {
+		const threshold: number = 0;
+		let lastScrollY = window.pageYOffset;
+		let ticking: boolean = false;
+		const updateScrollDir = () => {
+			const scrollY = window.pageYOffset;
+
+			if (Math.abs(scrollY - lastScrollY) < threshold) {
+				ticking = false;
+				return;
+			}
+			setScrollDir(scrollY > lastScrollY ? "down" : "up");
+			lastScrollY = scrollY > 0 ? scrollY : 0;
+			ticking = false;
+		};
+
+		const onScroll = () => {
+			if (!ticking) {
+				window.requestAnimationFrame(updateScrollDir);
+				ticking = true;
+			}
+		};
+
+		window.addEventListener("scroll", onScroll);
+
+		return () => window.removeEventListener("scroll", onScroll);
+	}, [scrollDir]);
+
 	return (
-		<div className="navbar">
-			Yo
-		</div>
-	)
+		<>
+			<div
+				className={
+					!scrollDir
+						? "nav navbarFadeIn"
+						: scrollDir === "down"
+							? "nav scrolledNav"
+							: "nav scrolledNavUp"
+				}
+			>
+				<input type="checkbox" id="nav-check" />
+
+				<div className={isHamburgerPressed === false ? "nav-links rolledUpNavLinks" : "nav-links"} onClick={() => setIsHamburgerPressed(false)}>
+					<a href="#">Home</a>
+					<a href="#">Events</a>
+					<a href="#">Gallery</a>
+					<div className="logoWrapper">
+						<div className='logo'>
+							<img
+								src="https://www.upload.ee/image/14420032/museum-icon-12886.png"
+								alt="museum icon"
+								id="museum-logo" />
+							<p>Vani Museum</p>
+						</div>
+					</div>
+					<a href="#">Updates</a>
+					<a href="#">History</a>
+					<a href="#">Contact</a>
+				</div>
+				<div className="nav-btn" onClick={() => hamburgerPressFunction()}>
+					<label htmlFor="nav-check">
+						<span></span>
+						<span></span>
+						<span></span>
+					</label>
+				</div>
+			</div>
+		</>
+	);
 }
